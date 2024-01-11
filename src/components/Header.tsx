7690165e47d807ce9119aaeb944f2df4dde69330
Alireza <alireza83safarii@gmail.com>
2024-01-12 00:40:20 +0330

    feat: add search query

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,90 +1,69 @@
 "use client";
-import {
-  FaAngleDown,
-  FaHamburger,
-  FaSearch,
-  FaSignInAlt,
-  FaSignOutAlt,
-} from "react-icons/fa";
-import { useState } from "react";
-import Image from "next/image";
-import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import Image from "next/image";
+import { FaPlus, FaSearch, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { signOut, useSession } from "next-auth/react";
+import { CiMenuBurger } from "react-icons/ci";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q");
   const { data: session } = useSession();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (q) {
+      setSearchTerm(q);
+    }
+  }, [q]);
 
   return (
     <header className="xl:container mx-auto md:px-8 px-2 z-20 border-b border-borderColor min-w-full sticky top-0 backdrop-blur-2xl">
       <div className="flex justify-between h-[90px] items-center text-gray-200">
         <div className="items-center md:flex hidden">
-          <Image
-            src="https://res.cloudinary.com/dmywzd0yw/image/upload/v1704525608/logo-white_dstkvu.png"
-            alt="logo"
-            className="2xl:max-h-[35px] 2xl:max-w-[100px] max-h-[44px] ml-3 sm:flex hidden"
-            width={100}
-            height={100}
-          />
+          <Link href="/home">
+            <Image
+              src="https://res.cloudinary.com/dmywzd0yw/image/upload/v1704525608/logo-white_dstkvu.png"
+              alt="logo"
+              className="2xl:max-h-[35px] 2xl:max-w-[100px] max-h-[44px] ml-3 sm:flex hidden"
+              width={100}
+              height={200}
+            />
+          </Link>
 
           <div className="lg:flex items-center font-semibold hidden">
-            <div className="relative group xl:mx-5 mx-2">
-              <button className="flex items-center">
-                <p>صفحه اصلی</p>
-                <FaAngleDown className="mt-1" />
-              </button>
-              <ul className="hidden group-hover:block absolute">
-                <li>test1</li>
-                <li>test2</li>
-                <li>test3</li>
-                <li>test4</li>
-                <li>test5</li>
-              </ul>
-            </div>
+            <Link
+              href="/nft"
+              className="flex xl:mx-5 mx-2 hover:text-blue duration-300"
+            >
+              فروشگاه
+            </Link>
 
-            <p className="mx-7">درباره</p>
+            <Link
+              href="/about"
+              className="flex xl:mx-5 mx-2 hover:text-blue duration-300"
+            >
+              درباره ما
+            </Link>
 
-            <div className="relative group xl:mx-5 mx-2">
-              <button className="flex items-center">
-                <p className="">مثال</p>
-                <FaAngleDown className="mt-1" />
-              </button>
-              <ul className="hidden group-hover:block absolute">
-                <li>test1</li>
-                <li>test2</li>
-                <li>test3</li>
-                <li>test4</li>
-                <li>test5</li>
-              </ul>
-            </div>
+            <Link
+              href="/contact-us"
+              className="flex xl:mx-5 mx-2 hover:text-blue duration-300"
+            >
+              ارتباط با ما
+            </Link>
 
-            <div className="relative group xl:mx-5 mx-2">
-              <button className="flex items-center">
-                <p className="">صفحات</p>
-                <FaAngleDown className="mt-1" />
-              </button>
-              <ul className="hidden group-hover:block absolute">
-                <li>test1</li>
-                <li>test2</li>
-                <li>test3</li>
-                <li>test4</li>
-                <li>test5</li>
-              </ul>
-            </div>
-
-            <div className="relative group xl:mx-5 mx-2">
-              <button className="flex items-center">
-                <p className="">وبلاگ</p>
-                <FaAngleDown className="mt-1" />
-              </button>
-              <ul className="hidden group-hover:block absolute">
-                <li>test1</li>
-                <li>test2</li>
-                <li>test3</li>
-                <li>test4</li>
-                <li>test5</li>
-              </ul>
-            </div>
+            <Link
+              href="/blog"
+              className="flex xl:mx-5 mx-2 hover:text-blue duration-300"
+            >
+              وبلاگ
+            </Link>
           </div>
         </div>
 
@@ -94,8 +73,13 @@ const Header = () => {
               type="text"
               className="border border-borderColor py-2 lg:py-3 px-4 bg-backgroundBlack rounded-lg lg:w-80 sm:w-60 w-48"
               placeholder="اینجا بگردید"
+              onChange={(e) => setSearchTerm(e.target.value)}
+              value={searchTerm}
             />
-            <button className="absolute left-3 top-3 lg:top-4">
+            <button
+              className="absolute left-3 top-3 lg:top-4"
+              onClick={() => push(`/nft?q=${searchTerm}`)}
+            >
               <FaSearch className="text-lg" />
             </button>
           </div>
@@ -112,13 +96,19 @@ const Header = () => {
             </button>
 
             <button
-              className={`rounded-full border border-borderColor sm:w-12 w-10 sm:h-12 h-10  justify-center items-center ${
-                session ? `flex` : `hidden`
-              }`}
+              className="rounded-full border border-borderColor sm:w-12 w-10 sm:h-12 h-10  justify-center items-center lg:hidden flex"
               onClick={() => setShowMenu(true)}
             >
-              <FaHamburger className="text-xl" />
+              <CiMenuBurger className="text-xl" />
             </button>
+
+            <Link
+              href="/create-nft"
+              className="rounded-full text-white hover:bg-gray-200 duration-300 bg-blue sm:w-12 w-10 sm:h-12 h-10 justify-center items-center flex"
+              onClick={() => setShowMenu(true)}
+            >
+              <FaPlus className="text-xl" />
+            </Link>
 
             <button
               className={`rounded-full border border-borderColor sm:w-12 w-10 sm:h-12 h-10  justify-center items-center ${
@@ -142,30 +132,46 @@ const Header = () => {
       </div>
 
       {showMenu && (
-        <div className="lg:hidden fixed top-0 left-0 min-h-full bg-[#242435] text-white border-r-2 border-borderColor">
+        <div className="lg:hidden fixed top-0 left-0 bg-[#242435] text-white border-r-2 border-borderColor min-h-screen">
           <div className="p-6 rounded-lg">
             <div className="flex flex-col space-y-4">
               <button
-                className="mt-4 rounded-full border border-borderColor w-8 h-8 flex justify-center items-center"
+                className="mt- rounded-full border border-borderColor w-8 h-8 flex justify-center items-center"
                 onClick={() => setShowMenu(false)}
               >
                 X
               </button>
-              <p className=" pb-5 text-center border-b border-borderColor">
-                صفحه اصلی
-              </p>
-              <p className=" pb-5 text-center border-b border-borderColor">
-                درباره
-              </p>
-              <p className=" pb-5 text-center border-b border-borderColor">
-                مثال
-              </p>
-              <p className=" pb-5 text-center border-b border-borderColor">
-                صفحات
-              </p>
-              <p className=" pb-5 text-center border-b border-borderColor">
+              <Link
+                href="/nft"
+                className=" pb-5 text-center border-b border-borderColor hover:text-blue duration-300"
+              >
+                فروشگاه
+              </Link>
+              <Link
+                href="/about"
+                className=" pb-5 text-center border-b border-borderColor hover:text-blue duration-300"
+              >
+                درباره ما
+              </Link>
+              <Link
+                href="/contact-us"
+                className=" pb-5 text-center border-b border-borderColor hover:text-blue duration-300"
+              >
+                ارتباط با ما
+              </Link>
+              <Link
+                href="/create-nft"
+                className=" pb-5 text-center border-b border-borderColor hover:text-blue duration-300"
+              >
+                افزودن ان اف تی
+              </Link>
+
+              <Link
+                href="/blog"
+                className=" pb-5 text-center border-b border-borderColor hover:text-blue duration-300"
+              >
                 وبلاگ
-              </p>
+              </Link>
             </div>
           </div>
         </div>
