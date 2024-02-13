466ed11de2653083f74a2450b3cc84ad62ddbbd9
Alireza <alireza83safarii@gmail.com>
2024-02-13 11:46:11 +0330

    feat: complate SaleGuide

--- src/app/home/@saleGuide/page.tsx
+++ src/app/home/@saleGuide/page.tsx
@@ -4,6 +4,37 @@ import Image from "next/image";
 import { FaAngleLeft } from "react-icons/fa6";
 import "swiper/css";
 import "swiper/css/bundle";
+interface TemplateProps {
+  img: string;
+  level: string;
+  title: string;
+  desc: string;
+}
+
+const Template: React.FC<TemplateProps> = ({ img, level, title, desc }) => {
+  return (
+    <div
+      className="xl:w-[23rem] lg:w-[20rem] md:w-[14rem] bg-[#242435] relative text-gray-200 p-4 md:my-0 my-5 mx-auto rounded-lg hover:text-blue duration-300"
+      data-aos="fade-up"
+    >
+      <div className="flex justify-between">
+        <Image
+          src={img}
+          alt="Portfolio Image"
+          width={70}
+          height={70}
+          className=" absolute -top-4 -left-3"
+        />
+        <p className="text-white mt-4">مرحله-{level}</p>
+      </div>
+      <p className="text-2xl font-bold mt-9">
+      <span className="transition-colors">{title}</span>
+      </p>
+      <p className="mt-7">{desc}</p>
+      <FaAngleLeft className="text-3xl mt-8" />
+    </div>
+  );
+};
 
 const SaleGuide = () => {
   return (
@@ -12,27 +43,27 @@ const SaleGuide = () => {
         NFT های خود را ایجاد و بفروشید
       </p>
       <div className="grid md:grid-cols-3 ">
-        <div
-          className=" xl:w-[23rem] lg:w-[20rem] md:w-[14rem] bg-[#242435] relative text-gray-200 p-4 md:my-0 my-5 mx-auto"
-          data-aos="fade-up"
-        >
-          <div className="flex justify-between">
-            <Image
-              src="https://res.cloudinary.com/dmywzd0yw/image/upload/v1704537227/shape-1_pmifkq.png"
-              alt="Portfolio Image"
-              width={70}
-              height={70}
-              className=" absolute -top-4 -left-3"
-            />
-            <p className="text-white mt-4">مرحله-02</p>
-          </div>
-          <p className="text-2xl font-bold mt-9">مجموعه خود را ایجاد کنید</p>
-          <p className="mt-7">
-            ما الگو را کاملاً پاسخگو کرده ایم، بنابراین عالی به نظر می رسد همه
-            دستگاه ها: دسکتاپ، تبلت و.
-          </p>
-          <FaAngleLeft className="text-3xl mt-8" />
-        </div>
+        <Template
+          img="/image/shape-7.png"
+          level="01"
+          title="کیف پول خود را تنظیم کنید"
+          desc="ویژگی‌ها و اجزای قدرتمند، که نورون را متمایز می‌کند،
+به راحتی قابل تنظیم و مقیاس پذیر است."
+        />
+        <Template
+          img="/image/shape-1.png"
+          level="02"
+          title="مجموعه خود را ایجاد کنید"
+          desc="مجموعه ای عالی از قالب های زیبای وب سایت برای نیاز شما.
+بهترین الگوی مناسب را انتخاب کنید."
+        />
+        <Template
+          img="/image/shape-5.png"
+          level="03"
+          title="NFT خود را اضافه کنید"
+          desc="ما الگو را کاملاً پاسخگو کرده ایم، بنابراین عالی به نظر می رسد
+همه دستگاه ها: دسکتاپ، تبلت و."
+        />
       </div>
     </div>
   );
